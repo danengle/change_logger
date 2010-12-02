153cc472f71d4c0888f451c7f45335408cdeb7f3
Dan Engle <dengle@cashnetusa.com>
2010-12-01 21:24:47 -0600

    finally got has_and_belongs_to_many record changes working correctly

--- lib/change_logger/acts_as_change_logger.rb
+++ lib/change_logger/acts_as_change_logger.rb
@@ -18,29 +18,30 @@ module ChangeLogger
         has_many :change_logs, :as => :item, :order => 'change_logs.created_at desc'
         after_save :record_attribute_updates
         after_destroy :record_object_destruction
-        self.class.reflections.each do |reflection|
-          if reflection[:macro] == :has_and_belongs_to_many
-            reflection.options.merge!({ :after_add => :record_association_add })
-            reflection.options.merge!({ :after_remove => :record_association_remove })
+        self.reflect_on_all_associations(:has_and_belongs_to_many).each do |reflection|
+          if reflection.options.include?(:after_add) || reflection.options.include?(:before_add)
+            logger.warn { "WARNING: change_logger adds after_add and after_remove options. You need to combine your current methods with the record_association_* methods in order for change_logger to work correctly." }
           end
+          new_options = { :after_add => :record_association_add, :after_remove => :record_association_remove }.merge(reflection.options)
+          has_and_belongs_to_many reflection.name.to_sym, new_options          
         end
       end
     end
     
     module InstanceMethods
+      
       def record_association_add(object)
+        puts "****: record_association_add object: #{object.inspect}"
         record_change(object.class.to_s, ACTIONS[:create], object.id)
       end
       
       def record_association_remove(object)
-        record_change(oject.class.to_s, object.id, ACTIONS[:delete])
+        record_change(object.class.to_s, object.id, ACTIONS[:delete])
       end
       
       def record_attribute_updates
-        attributes.delete_if{|k,v| self.class.ignore.include?(k) }.each do |key, value|
-          if send("#{key}_changed?")
-            record_change(key, old_value(key), send(key))
-          end
+        self.changes.delete_if { |k,v| self.class.ignore.include?(k) }.each do |key, value|
+          record_change(key, value[0], value[1])
         end
       end
 
