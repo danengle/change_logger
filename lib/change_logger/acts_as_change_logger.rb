f98d277c15755e3654837f705b9badcf8e411ed2
Dan Engle <dengle@cashnetusa.com>
2011-03-09 14:28:23 -0600

    added templates...still needs work though

--- lib/change_logger/acts_as_change_logger.rb
+++ lib/change_logger/acts_as_change_logger.rb
@@ -2,6 +2,7 @@ module ChangeLogger
   module ActsAsChangeLogger
     ACTIONS = {
       :create => 'CREATED',
+      :update => 'UPDATED',
       :delete => 'DELETED'
     }
     def self.included(base)
@@ -11,37 +12,61 @@ module ChangeLogger
     module ClassMethods
       def acts_as_change_logger(options = {})
         send :include, InstanceMethods
-        cattr_accessor :ignore, :templates
-        self.ignore = (options[:ignore] || []).map &:to_s
-        self.ignore.push('id', 'created_at', 'updated_at')
         
-        self.templates = (options[:track_templates] || []).map &:to_s
-        has_many :template_logs, :as => :item, :order => 'template_logs.created_at desc'
+        cattr_accessor :ignore, :track_templates
+        self.ignore = (options[:ignore] || []).map &:to_s
+        self.ignore.push('id', 'revision', 'created_at', 'updated_at')
+        self.track_templates = (options[:track_templates] || []).map &:to_s
+        
+        attr_accessor :template_changed
+        after_update :record_template_change
         
         has_many :change_logs, :as => :item, :order => 'change_logs.created_at desc'
         after_create :record_object_creation
+        before_update :increment_revision
         after_update :record_attribute_updates
         before_destroy :record_object_destruction
         self.reflect_on_all_associations(:has_and_belongs_to_many).each do |reflection|
-          unless self.templates.include?(reflection.name.to_sym)
-            if reflection.options.keys.include?(:after_add) || reflection.options.keys.include?(:before_add)
-              logger.warn { "WARNING: change_logger adds after_add and after_remove options to has_and_belongs_to_many relationships. You need to combine your current methods with the record_association_* methods in order for change_logger to work correctly." }
-            end
-            new_options = { :after_add => :record_association_add, :after_remove => :record_association_remove }.merge(reflection.options)
-            has_and_belongs_to_many reflection.name.to_sym, new_options
+          if reflection.options.keys.include?(:after_add) || reflection.options.keys.include?(:before_add)
+            logger.warn { "WARNING: change_logger adds after_add and after_remove options to has_and_belongs_to_many relationships. You need to combine your current methods with the record_association_* methods in order for change_logger to work correctly." }
           end
+          new_options = { :after_add => :record_association_add, :after_remove => :record_association_remove }.merge(reflection.options)
+          has_and_belongs_to_many reflection.name.to_sym, new_options
         end
       end
     end
     
     module InstanceMethods
       
+      def increment_revision
+        self.increment(:revision) if self.respond_to?(:revision)
+      end
+      
+      def record_template_change
+        self.template_changed = {} if self.template_changed.nil?
+        if self.template_changed.values.include?(true)
+          self.template_changed.keys.each do |relation|
+            record_change("#{relation}_template", ACTIONS[:update], self.send(relation).to_yaml)
+          end
+        end
+      end
+      
       def record_association_add(object)
-        record_change(object.class.to_s, ACTIONS[:create], object.id) if self.persisted?
+        if self.class.track_templates.include?(object.class.to_s.tableize)
+          self.template_changed = {} if self.template_changed.nil?
+          self.template_changed[object.class.to_s.tableize.to_sym] = true          
+        else
+          record_change(object.class.to_s, ACTIONS[:create], object.id) if self.persisted?
+        end
       end
       
       def record_association_remove(object)
-        record_change(object.class.to_s, object.id, ACTIONS[:delete]) if self.persisted?
+        if self.class.track_templates.include?(object.class.to_s.tableize)
+          self.template_changed = {} if self.template_changed.nil?
+          self.template_changed[object.class.to_s.tableize.to_sym] = true          
+        else
+          record_change(object.class.to_s, object.id, ACTIONS[:delete]) if self.persisted?
+        end
       end
       
       def record_object_creation
@@ -69,13 +94,16 @@ module ChangeLogger
       private
       
       def record_change(attribute_name, old_val, new_val)        
-        self.change_logs.create(
+        change_log = self.change_logs.new(
           :attribute_name => attribute_name,
           :old_value => old_val,
           :new_value => new_val,
           :changed_by => whodunnit
         )
+        change_log.revision = self.revision if self.respond_to?(:revision)
+        change_log.save
       end
+            
     end
   end
 end
