469142564fada858624391d655eb61cb980f214d
Dan Engle <dengle@cashnetusa.com>
2011-03-09 14:49:44 -0600

    forgot to add value method

--- lib/app/models/change_log.rb
+++ lib/app/models/change_log.rb
@@ -1,4 +1,14 @@
 class ChangeLog < ActiveRecord::Base
   belongs_to :item, :polymorphic => true
   validates_presence_of :item_id, :item_type, :attribute_name
+  
+  def value
+    # TODO find out if this is the best way to loan an attribute that may
+    # or may not be yaml
+    begin
+      YAML.load(self.new_value)
+    rescue
+      self.new_value
+    end
+  end
 end
